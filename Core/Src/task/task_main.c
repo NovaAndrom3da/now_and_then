27051e5787e5b5be2ea32f374bfd66a7e439ac37
Gijs Landwehr <gijslandwehr@utexas.edu>
2021-05-18 15:33:59 -0500

    make main task dump out dummy message over can

--- Core/Src/task/task_main.c
+++ Core/Src/task/task_main.c
@@ -12,12 +12,20 @@ uint32_t counter = 0;
 _Noreturn void start_task_main(void *argument) {
     BaseType_t status = 0;
     IMD_message data = {0};
+    uint8_t dummy = 0x13;
 
+    TickType_t last_wake = xTaskGetTickCount();
 
     while (1) {
-        status = xQueueReceive(IMD_Q, &data, portMAX_DELAY);
+        status = xQueueReceive(IMD_Q, &data, 0);
         if (status == pdPASS) {
             send_can_msg(0x69, &data.frequency, sizeof(data.frequency));
         }
+
+        send_can_msg(0x01, &dummy, 1);
+
+
+
+        vTaskDelayUntil(&last_wake, pdMS_TO_TICKS(100));
     }
 }
